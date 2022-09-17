5ae560ffa5ad5f05235dc3e8d3b3d6df9907a6e7
filip <filip@digihood.cz>
2022-09-17 13:38:04 +0200

    přidan command

--- src/app/console/Commands/InstallBranding.php
+++ src/app/console/Commands/InstallBranding.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 class InstallBranding extends Command
 {
-    use Traits\DigiCommandOutput;
+    use \Backpack\CRUD\app\Console\Commands\Traits\DigiCommandOutput;
     /**
      * The name and signature of the console command.
      *
