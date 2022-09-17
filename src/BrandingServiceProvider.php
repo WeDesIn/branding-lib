215805719d929275166edb1ff14b5e5351ccb02f
filip <filip@digihood.cz>
2022-09-17 12:30:41 +0200

    uprava načítaní routes

--- src/BrandingServiceProvider.php
+++ src/BrandingServiceProvider.php
@@ -4,16 +4,32 @@ namespace Digihood\Branding;
 
 use Illuminate\Support\ServiceProvider;
 
-
-
-    
 class BrandingServiceProvider extends ServiceProvider {
+
+
+    public $routeFilePath = '/routes/web.php';
+
         public function boot()
         {
-            $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+            $this->setupCustomRoutes($this->app->router);
         }
         public function register()
         {
 
         }
+
+
+            /**
+     * Load custom routes file.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function setupCustomRoutes(Router $router)
+    {
+        // if the custom routes file is published, register its routes
+        if (file_exists(base_path().$this->customRoutesFilePath)) {
+            $this->loadRoutesFrom(base_path().$this->customRoutesFilePath);
+        }
+    }
     }
